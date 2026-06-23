3db08b43aa4fefa645199531b9cea5d817bab67c
sametyildirim314 <sametyldrmckrxwz@gmail.com>
2026-06-23 21:38:17 +0300

    user table  updated

--- Migrations/20260623182732_KullaniciLoginAlanlari.Designer.cs
+++ Migrations/20260623182732_KullaniciLoginAlanlari.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniCareer.SimpleAPI.Data;
 
@@ -11,9 +12,11 @@ using UniCareer.SimpleAPI.Data;
 namespace CVGenius_AI.Migrations
 {
     [DbContext(typeof(CvContext))]
-    partial class CvContextModelSnapshot : ModelSnapshot
+    [Migration("20260623182732_KullaniciLoginAlanlari")]
+    partial class KullaniciLoginAlanlari
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
